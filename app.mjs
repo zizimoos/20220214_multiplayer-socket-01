0ce1f38c298c9b88634e648f01c9c5c0e6be7dc5
henry <henry@iMac-home.local>
2022-02-16 08:49:51 +0900

    wow..it's not easy...

--- app.mjs
+++ app.mjs
@@ -19,22 +19,55 @@ app.use(express.static(__dirname + "/"));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 ///////////////////////////////////////////////////////////////////////////////
+import Coin from "./js/coin.mjs";
 
-const players = [];
+let players = [];
+let coins = [];
+
+for (let i = 0; i < 20; i++) {
+  coins.push(
+    new Coin({ id: i, x: Math.random() * 800, y: Math.random() * 600 })
+  );
+}
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
-  socket.emit("init", { id: socket.id, plyrs: players });
+  socket.emit("init", { id: socket.id, plyrs: players, coins });
 
-  socket.on("newPlayer", (obj) => {
+  socket.on("new-player", (obj) => {
+    obj.id = socket.id;
     players.push(obj);
     console.log("server player array", players.length);
-    socket.broadcast.emit("newPlayer", obj);
+    socket.broadcast.emit("new-player", obj);
   });
+
   socket.on("move-player", (dir) =>
     socket.broadcast.emit("move-player", { id: socket.id, dir })
   );
   socket.on("stop-player", (dir) =>
     socket.broadcast.emit("stop-player", { id: socket.id, dir })
   );
+
+  socket.on("destroy-item", ({ playerId, coinId }) => {
+    if (coins.find((v) => v.id === coinId)) {
+      const player = players.find((v) => v.id === playerId);
+      const sock = io.sockets.sockets.get(player.id);
+      coins = coins.filter((v) => v.id !== coinId);
+      player.xp += 10;
+      socket.broadcast.emit("destroy-item", { coinId });
+
+      sock.emit("update-player", player);
+
+      if (player.xp === 100) {
+        sock.emit("end-game", "win");
+        sock.broadcast.emit("end-game", "lose");
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    players = players.filter((v) => v.id !== socket.id);
+    socket.broadcast.emit("remove-player", { id: socket.id });
+  });
 });
--- js/main.mjs
+++ js/main.mjs
@@ -1,26 +1,23 @@
 import Player from "./player.mjs";
 import controls from "./controls.mjs";
+import Coin from "./coin.mjs";
 
 const socket = io();
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const writeToCanvas = (msg) => {
-  ctx.fillStyle = "white";
-  ctx.font = "30px Arial";
-  ctx.fillText(msg, 30, 30);
-};
-
 let players = [];
+let items = [];
+let endGame;
 
-socket.on("init", ({ id, plyrs }) => {
-  writeToCanvas(`Welcome to the game ${id}`);
-
-  const player = new Player({ id });
+socket.on("init", ({ id, plyrs, coins }) => {
+  const player = new Player({ id, main: true });
   controls(player, socket);
+  console.log(player);
+  socket.emit("new-player", player);
 
-  socket.emit("newPlayer", player);
-  socket.on("newPlayer", (obj) => players.push(new Player(obj)));
+  socket.on("new-player", (obj) => players.push(new Player(obj)));
+
   socket.on("move-player", ({ id, dir }) =>
     players.find((v) => v.id === id).move(dir)
   );
@@ -28,15 +25,47 @@ socket.on("init", ({ id, plyrs }) => {
     players.find((v) => v.id === id).stop(dir)
   );
 
+  socket.on("destroy-item", (id) => (items = items.filter((v) => v.id !== id)));
+  socket.on(
+    "remove-player",
+    (id) => (players = players.filter((v) => v.id !== id))
+  );
+
+  socket.on("end-game", (result) => (endGame = result));
+  socket.on("update-player", (obj) => (player.xp = obj.xp));
+
+  ////////////////////////////////////////////////////////////////////////////////////
+
   players = plyrs.map((v) => new Player(v)).concat(player);
-  console.log("client player array", players);
+  items = coins.map((v) => new Coin(v));
+
+  ////////////////////////////////////////////////////////////////////////////////////
 
   const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    players.forEach((player) => {
-      player.draw(ctx);
+
+    players.forEach((v) => v.draw(ctx, items));
+
+    items.forEach((v) => {
+      v.draw(ctx);
+      if (v.destroyed) {
+        socket.emit("destroy-item", { playerId: v.destroyed, coinId: v.id });
+        console.log("destroyed", v.destroyed);
+      }
     });
-    requestAnimationFrame(draw);
+
+    items = items.filter((v) => !v.destroyed);
+
+    if (endGame) {
+      ctx.fillStyle = endGame === "win" ? "green" : "red";
+      ctx.font = "100px Arial";
+      ctx.fillText(`YOU ${endGame}`, 100, 100);
+    }
+
+    !endGame && requestAnimationFrame(draw);
   };
+
+  ////////////////////////////////////////////////////////////////////////////////////
+
   draw();
 });
